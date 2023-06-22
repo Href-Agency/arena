1ebb69c340903249a07658bcefcb5654d829e2fd
RadekHref <galek.radek@icloud.com>
2023-06-22 13:26:22 +0100

    extra data aos

--- wp-content/themes/arena/page.php
+++ wp-content/themes/arena/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <article <?php post_class('blocks'); ?> id="smooth-content">
-  <div class="min-h-[100vh]">
+  <div class="min-h-[100vh] -mb-1">
     <?php 
       if (have_posts()) : while (have_posts()) : the_post();
         the_content(); 
